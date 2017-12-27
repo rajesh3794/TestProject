1d9cd717622c4a1cb7e354a12734b63b457bcc4c
rajeshdronavalli <rajeshimpeccable@gmail.com>
2017-12-27 09:18:37 -0800

    added for loop logic

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -4,6 +4,11 @@ public class Test {
 	public static void main(String[] args)
 	{
 		System.out.println("test case passed");
+		
+		for(int i=0; i<5; i++)
+		{
+			System.out.println("printed " +i);
+		}
 
 	}
 
